2c5ca854d53c39259cfdaf18a20681380f75483e
Avinash <guda.reddy@iitgn.ac.in>
2025-11-08 16:59:33 +0530

    Added nigg folder

---
+++ nigg/praneeth/Constructor_Demo/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class ObjectTracker
+{
+    private int data;
+    private static int activeObjects = 0;
+
+    public ObjectTracker()
+    {
+        activeObjects++;
+        Console.WriteLine($"Constructor Called. Active Objects: {activeObjects}");
+    }
+
+    ~ObjectTracker()
+    {
+        activeObjects--;
+        Console.WriteLine($"Object Destroyed. Active Objects: {activeObjects}");
+    }
+
+    public void SetData(int x)
+    {
+        data = x;
+    }
+
+    public void ShowData()
+    {
+        Console.WriteLine($"Data: {data}");
+    }
+
+    public static int GetActiveObjects()
+    {
+        return activeObjects;
+    }
+}
+
+class Program
+{
+    static void CreateAndDestroyObjects()
+    {
+        ObjectTracker obj1 = new ObjectTracker();
+        obj1.SetData(10);
+
+        ObjectTracker obj2 = new ObjectTracker();
+        obj2.SetData(20);
+
+        ObjectTracker obj3 = new ObjectTracker();
+        obj3.SetData(30);
+
+        Console.WriteLine("\n--- Displaying Data ---");
+        obj1.ShowData();
+        obj2.ShowData();
+        obj3.ShowData();
+    }
+
+    static void Main()
+    {
+        CreateAndDestroyObjects();
+
+        // Force garbage collection to call destructors
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+
+        Console.WriteLine("\nProgram Ended.");
+    }
+}
+
---
+++ nigg/praneeth/Inheritance_Demo/Program.cs
@@ -0,0 +1,88 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Vehicle
+{
+    protected int speed;
+    protected int fuel;
+
+    public Vehicle(int speed, int fuel)
+    {
+        this.speed = speed;
+        this.fuel = fuel;
+    }
+
+    public virtual void Drive()
+    {
+        fuel -= 5;
+        Console.WriteLine("Vehicle is moving...");
+    }
+
+    public virtual void ShowInfo()
+    {
+        Console.WriteLine($"Speed: {speed}, Fuel: {fuel}");
+    }
+}
+
+class Car : Vehicle
+{
+    private int passengers;
+
+    public Car(int speed, int fuel, int passengers)
+        : base(speed, fuel)
+    {
+        this.passengers = passengers;
+    }
+
+    public override void Drive()
+    {
+        fuel -= 10;
+        Console.WriteLine("Car is moving with passengers...");
+    }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Speed: {speed}, Fuel: {fuel}, Passengers: {passengers}");
+    }
+}
+
+class Truck : Vehicle
+{
+    private int cargoWeight;
+
+    public Truck(int speed, int fuel, int cargoWeight)
+        : base(speed, fuel)
+    {
+        this.cargoWeight = cargoWeight;
+    }
+
+    public override void Drive()
+    {
+        fuel -= 15;
+        Console.WriteLine("Truck is moving with cargo...");
+    }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Speed: {speed}, Fuel: {fuel}, Cargo Weight: {cargoWeight}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Vehicle[] vehicles = new Vehicle[3];
+        vehicles[0] = new Vehicle(80, 100);
+        vehicles[1] = new Car(100, 120, 4);
+        vehicles[2] = new Truck(60, 150, 2000);
+
+        Console.WriteLine("\n--- Demonstrating Polymorphism ---");
+        foreach (Vehicle v in vehicles)
+        {
+            v.Drive();
+            v.ShowInfo();
+            Console.WriteLine();
+        }
+    }
+}
